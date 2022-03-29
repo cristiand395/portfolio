969cfb4eacd2a0d70a89c8292f5ac7ffb2a1be99
cristiand395 <cristiand395@gmail.com>
2022-03-29 19:10:12 +0200

    updating projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -53,12 +53,6 @@ const socialMediaLinks = [
     backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
   },
   {
-    name: "YouTube",
-    link: "https://www.youtube.com/channel/UC_amoXmmxSY9KusoDczDTXQ",
-    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
-    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
-  },
-  {
     name: "Behance",
     link: "https://www.behance.net/cristiandavila",
     fontAwesomeIcon: "fa-behance", // Reference https://fontawesome.com/icons/youtube?style=brands
